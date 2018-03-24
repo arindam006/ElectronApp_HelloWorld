92112f6e03f1ad6ae6d8846332cfd24a5ae09b64
Arindam Roy <arindamroy@outlook.in>
2018-03-24 20:42:12 +0530

    Adding configuration files using config module

--- HelloWorld/main.js
+++ HelloWorld/main.js
@@ -2,16 +2,19 @@ const electron = require('electron');
 const app = electron.app;
 const path = require('path');
 const url = require('url');
+const config = require('config');
 
 const BrowserWindow = electron.BrowserWindow;
 
 var mainWindow;
 
+var bgCol = config.get("App.BackgroundColor");
+
 app.on('ready', function(){
   mainWindow = new BrowserWindow({
     width:1024,
     height:768,
-    backgroundColor:'#ffffff'
+    backgroundColor: bgCol
   });
 
   //opens a browser window
@@ -19,7 +22,7 @@ app.on('ready', function(){
 
   //renders html page
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'contents/html/index.html'),
+    pathname: path.join(__dirname, 'contents/html/test.html'),
     protocol: 'file:',
     slashes: true
   }));
